8a662b7e5da83e8e1454034a81da0116a06db987
Nadira Fawziyya Masnur <68457610+458-nadiraF@users.noreply.github.com>
2024-11-24 16:29:55 +0700

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({"message": "POST received"}).encode())
