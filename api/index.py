fec9171f44472edeccf61333db3b30dc169a5805
Nadira Fawziyya Masnur <68457610+458-nadiraF@users.noreply.github.com>
2024-11-25 13:38:04 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -41,7 +41,7 @@ class handler(BaseHTTPRequestHandler):
             # Get the content length to read the request body
             content_length = int(self.headers.get('Content-Length', 0))
             post_data = self.rfile.read(content_length).decode('utf-8')
-            data = json.loads(post_data)
+            #data = json.loads(post_data)
             received_json = post_data.json()
             #received_json = json.loads(post_data.decode('utf-8'))
             lot=received_json.get('lot')
